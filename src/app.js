c319ef8f4b03ed30047e552f9d3b00715a673819
John Balke <john.balke@gmail.com>
2021-03-16 15:59:04 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const logo = document.querySelector('.logo');
+const hamburger = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
 
 logo.innerHTML = logo.textContent
   .split('')
@@ -7,3 +9,33 @@ logo.innerHTML = logo.textContent
       `<span style="transition-delay: ${i * 300}ms">${character}</span>`
   )
   .join('');
+
+function toggleMobileMenu() {
+  hamburger.classList.toggle('active');
+  navList.classList.toggle('show');
+  document.body.classList.toggle('mobile-menu-open');
+
+  const menuItems = navList.querySelectorAll('#nav-list.show li');
+
+  menuItems.forEach((item) => {
+    item.addEventListener(
+      'click',
+      (event) => {
+        toggleMobileMenu();
+      },
+      { once: true }
+    );
+  });
+}
+
+hamburger.addEventListener('click', (event) => {
+  toggleMobileMenu();
+});
+
+VanillaTilt.init(document.querySelectorAll('.project'), {
+  max: 25,
+  speed: 400,
+  reverse: true,
+  max: 3,
+  scale: 1.05,
+});
